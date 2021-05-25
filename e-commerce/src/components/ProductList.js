83c37b90227cb328aa00db8d223371ee88efc2b8
JJKOrlino <jjkorlino80@yahoo.com>
2021-05-25 14:57:00 -0500

    Title and Product View

--- e-commerce/src/components/ProductList.js
+++ e-commerce/src/components/ProductList.js
@@ -4,7 +4,28 @@ import ProductItem from "./ProductItem";
 
 const ProductList = props => {
   const { products } = props.context;
-
+  return (
+    <>
+      <div className="hero is-primary">
+        <div className="hero-body container">
+          <h4 className="title">Flatiron Exclusive Merchandise</h4>
+        </div>
+      </div>
+      <div className="container">
+        <div className="column columns is-multiline">
+          {products && products.length ? (
+            products.map((product, index) => (
+              <ProductItem
+                product={product}
+                key={index}
+                addToCart={props.context.addToCart}
+              />
+            ))
+          ) : (
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
