ab7dfb7f847187c536eaca2fda2a74c472320813
JJKOrlino <jjkorlino80@yahoo.com>
2021-05-25 14:55:52 -0500

    ProductList

--- e-commerce/src/components/ProductList.js
+++ e-commerce/src/components/ProductList.js
@@ -5,33 +5,6 @@ import ProductItem from "./ProductItem";
 const ProductList = props => {
   const { products } = props.context;
 
-  return (
-    <>
-      <div className="hero is-primary">
-        <div className="hero-body container">
-          <h4 className="title">Flatiron Exclusive Merchandise</h4>
-        </div>
-      </div>
-      <div className="container">
-        <div className="column columns is-multiline">
-          {products && products.length ? (
-            products.map((product, index) => (
-              <ProductItem
-                product={product}
-                key={index}
-                addToCart={props.context.addToCart}
-              />
-            ))
-          ) : (
-            <div className="column">
-              <span className="title has-text-grey-light">
-                No products!
-              </span>
-            </div>
-          )}
-        </div>
-      </div>
-    </>
   );
 };
 
